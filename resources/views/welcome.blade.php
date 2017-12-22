7764e64af78b4cad2ca17ddef2eccd7a49d00045
wssl135 <wssl135@163.com>
2017-12-22 23:23:20 +0800

    edit welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>AiParty - We love party!</title>
+        <title>AiParty - We love to go to party!</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
